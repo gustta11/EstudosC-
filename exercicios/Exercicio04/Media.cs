935823083379b084dbf2bd4a1f66b72c9da3a903
gustta11 <gustavokeven50@gmail.com>
2026-08-08 17:57:13 -0300

    docs: adicionando exercicios

---
+++ exercicios/Exercicio04/Media.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Exercicio04
+{
+    class Media
+    {
+        public Media(double nota1, double nota2)
+        {
+            double media = (nota1 + nota2) / 2;
+
+            Console.WriteLine($"A media do aluno: {media}");
+        }
+
+        public Media(double nota1, double nota2, double nota3)
+        {
+            double media = (nota1 + nota2 + nota3) / 3;
+
+            Console.WriteLine($"A media do aluno: {media}");
+        }
+
+    }
+}
---
+++ exercicios/Exercicio04/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Exercicio04;
+
+public class Program
+{
+    public static void Main()
+    {
+        Media aluno = new(9.5, 8.5);
+        Media aluno2 = new(8.5, 7.5, 9.8);
+    }
+}
